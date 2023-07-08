ab1941d64b1a8a0353afefcb31a54d4a0605f812
antonpopov <aopopov90@gmail.com>
2023-07-08 11:14:13 +0100

    global exception handler

---
+++ src/main/java/com/home/reminisce/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package com.home.reminisce.exceptions;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException(EntityNotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(e.getMessage());
+    }
+}
